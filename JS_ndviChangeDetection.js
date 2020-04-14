269808158ac0407eba6f2846755b6f699abf588f
MoezLabiadh <60794110+MoezLabiadh@users.noreply.github.com>
2020-04-14 09:30:21 -0700

    Update JS_ndviChangeDetection.js

--- JS_ndviChangeDetection.js
+++ JS_ndviChangeDetection.js
@@ -31,73 +31,56 @@ function maskL8sr(image) {
   return image.updateMask(mask);
 }
 
-// Define NDVI and NDMI indices function for Landsat-5 collection 
-function L5_Indices (image) {
+// Define a function to caclulate NDVI for Landsat-5 collection 
+function L5_ndvi (image) {
     var l5_ndvi = image.normalizedDifference(['B4', 'B3']).rename('L5_NDVI');
-    var l5_ndmi = image.normalizedDifference(['B4', 'B5']).rename('L5_NDMI');
-    return image.addBands(l5_ndvi).addBands (l5_ndmi);
+    return image.addBands(l5_ndvi);
         }
 
-// Define NDVI and NDMI indices function for Landsat-8 collection 
-function L8_Indices(image) {
+// Define a function to caclulate NDVI for Landsat-8 collection 
+function L8_ndvi(image) {
     var l8_ndvi = image.normalizedDifference(['B5', 'B4']).rename('L8_NDVI');
-    var l8_ndmi = image.normalizedDifference(['B5', 'B6']).rename('L8_NDMI');    
-    return image.addBands(l8_ndvi).addBands (l8_ndmi);
+    return image.addBands(l8_ndvi);
         }        
 
-// Add Landsat-5 collection. Filter by date and AOI. Apply Cloud Mask and NDVI function        
+// Add Landsat-5 collection. Filter by date and AOI. Apply Cloud Mask and NDVI functions.        
 var bef_L5 = ee.ImageCollection('LANDSAT/LT05/C01/T1_SR')
                  .filterDate(bef_start_date, bef_end_date)
                  .filter(ee.Filter.calendarRange(7,8,'month'))
                  .filter(ee.Filter.lt('CLOUD_COVER', cloud_threshold))
                  .filterBounds(AOI)
                  .map(cloudMaskL5)
-                 .map(L5_Indices)
+                 .map(L5_ndvi);
                  
-// Add Landsat-8 collection. Filter by date and AOI. Apply Cloud Mask and NDVI function                       
+// Add Landsat-8 collection. Filter by date and AOI. Apply Cloud Mask and NDVI functions.                       
 var aft_L8 = ee.ImageCollection("LANDSAT/LC08/C01/T1_SR")
                  .filterDate(aft_start_date, aft_end_date)
                  .filter(ee.Filter.calendarRange(7,8,'month'))
                  .filter(ee.Filter.lt('CLOUD_COVER', cloud_threshold))
                  .filterBounds(AOI)
                  .map(maskL8sr)
-                 .map(L8_Indices);
+                 .map(L8_ndvi);
 print (bef_L5);
 print (aft_L8);
 
 // Compute statisctis (mean, StDev) and NDVI change/anomalie 
 var bef_mean_NDVI = bef_L5.select ('L5_NDVI').mean();
-var bef_mean_NDMI = bef_L5.select ('L5_NDMI').mean();
-
 var aft_mean_NDVI = aft_L8.select ('L8_NDVI').mean(); 
-var aft_mean_NDMI = aft_L8.select ('L8_NDMI').mean();
 var bef_stDev_NDVI = bef_L5.select ('L5_NDVI').reduce (ee.Reducer.stdDev());
 
-var ndviChange = ((aft_mean_NDVI.divide(bef_mean_NDVI))
-                                .multiply(100))
-                                .subtract(100)
-                                .rename('ndviChange').clip(AOI);
+var ndviChange = ((aft_mean_NDVI.subtract(bef_mean_NDVI))
+                                .divide(bef_mean_NDVI))
+                                .multiply(100)
+                                .rename('ndviChange')
+                                .clip(AOI);
 
-var ndmiChange = ((aft_mean_NDMI.divide(bef_mean_NDMI))
-                                .multiply(100))
-                                .subtract(100)
-                                .rename('ndmiChange').clip(AOI);
                                 
 var ndviAnomaly = ((aft_mean_NDVI.subtract(bef_mean_NDVI))
                                  .divide(bef_stDev_NDVI))
-                                 .rename ('ndviAnomaly').clip(AOI);
+                                 .rename ('ndviZscore')
+                                 .clip(AOI);
 
 //Set visualisation parameters
-var NdviVizParam = {
-  min: 0,
-  max: 1,
-  palette: [
-    'FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901',
-    '66A000', '529400', '3E8601', '207401', '056201', '004C00', '023B01',
-    '012E01', '011D01', '011301'
-  ],
-};
-
 var NdviChangeVizParam = {
   min: -50,
   max: 20,
@@ -105,21 +88,19 @@ var NdviChangeVizParam = {
 };
 
 var NdviAnomalyVizParam = {
-  min: -5,
-  max: 2,
+  min: -3,
+  max: 3,
   palette: ['purple','red','orange','yellow','green'],
 };
 
 //Add layers to the map
 Map.centerObject(AOI, 12);
-Map.addLayer(bef_mean_NDVI.clip(AOI), NdviVizParam, 'bef_ndvi');
-Map.addLayer(aft_mean_NDVI.clip(AOI), NdviVizParam, 'aft_ndvi');
-Map.addLayer(ndmiChange.clip(AOI), NdviChangeVizParam, 'ndmiChange');
+Map.addLayer(ndviAnomaly.clip(AOI), NdviAnomalyVizParam, 'ndviZscore');
 Map.addLayer(ndviChange.clip(AOI), NdviChangeVizParam, 'ndviChange');
-Map.addLayer(ndviAnomaly.clip(AOI), NdviAnomalyVizParam, 'ndviAnomaly');
 
-//Export outputs to Drive.
-/*
+
+//Export outputs to GDrive.
+
 Export.image.toDrive({
   image: ndviChange,
   description: 'NDVIchange_1990-2000_2018-2019_07-08',
@@ -129,15 +110,8 @@ Export.image.toDrive({
 
 Export.image.toDrive({
   image: ndviAnomaly,
-  description: 'NDVIanomaly_1990-2000_2018-2019_07-08',
+  description: 'ndviZscore_1990-2000_2018-2019_07-08',
   scale: 30,
   region: AOI
 });
 
-Export.image.toDrive({
-  image: ndmiChange,
-  description: 'NDMIchange_1990-2000_2018-2019_07-08',
-  scale: 30,
-  region: AOI
-});
-*/
